f525e9d7bb058bfb7f92ee125adf39ae6714c4ac
acro.mitani <mitani@acroquest.co.jp>
2020-09-29 18:31:21 +0900

    3.4.3

--- main.tf
+++ main.tf
@@ -2,11 +2,10 @@ provider "aws"{
   region = "ap-northeast-1"
 }
 
-variable "env" {}
-
 resource "aws_instance" "example" {
   ami = "ami-0f9ae750e8274075b"
-  instance_type = var.env == "prod" ? "m5.large" : "t3.micro"
+  instance_type = "t3.micro"
+  user_data = file("./user_data.sh")
 }
 
 output "example_public_dns" {
