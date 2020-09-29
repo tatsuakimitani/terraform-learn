fc3a6e8e0375cb6cbed8c954827c2fd9f8001099
acro.mitani <mitani@acroquest.co.jp>
2020-09-29 18:26:25 +0900

    3.2.4

--- main.tf
+++ main.tf
@@ -2,47 +2,11 @@ provider "aws"{
   region = "ap-northeast-1"
 }
 
-data "aws_ami" "recent_amazon_linux_2" {
-  most_recent = true
-  owners = ["amazon"]
-  filter {
-    name = "name"
-    values = ["amzn2-ami-hvm-2.0.????????-x86?64-gp2"]
-  }
+variable "env" {}
 
-  filter{
-    name = "state"
-    values = ["available"]
-  }
-}
-
-resource "aws_security_group" "example_ec2" {
-  name = "example-ec2"
-
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "example"{
-  ami = data.aws_ami.recent_amazon_linux_2.image_id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.example_ec2.id]
-  user_data = <<EOF
-    #!/bin/bash
-    yum install -y httpd
-    systemctl start httpd.service
-EOF
+resource "aws_instance" "example" {
+  ami = "ami-0f9ae750e8274075b"
+  instance_type = var.env == "prod" ? "m5.large" : "t3.micro"
 }
 
 output "example_public_dns" {
