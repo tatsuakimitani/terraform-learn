551df6e0cce9c47ec51e43e8a34f0fd51062f732
acro.mitani <mitani@acroquest.co.jp>
2020-09-30 16:06:28 +0900

    3.7

--- main.tf
+++ main.tf
@@ -2,20 +2,11 @@ provider "aws"{
   region = "ap-northeast-1"
 }
 
-data "template_file" "httpd_user_data" {
-  template = file("./user_data.sh.tpl")
-
-  vars = {
-    package = "httpd"
-  }
-}
-
-resource "aws_instance" "example" {
-  ami = "ami-0f9ae750e8274075b"
+module "dev_server" {
+  source = "./httpd_server"
   instance_type = "t3.micro"
-  user_data = data.template_file.httpd_user_data.rendered
 }
 
 output "example_public_dns" {
-  value       = aws_instance.example.public_dns
+  value       = module.dev_server.public_dns
 }
